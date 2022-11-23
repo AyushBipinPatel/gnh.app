a9277b01cd3d457de8d6dd1450062467e30400fe
AyushBipinPatel <ayush.ap58@gmail.com>
2022-11-23 18:11:16 +0530

    add argument to allow indicatro level colours

--- R/fct_hch_simple_column_chart.R
+++ R/fct_hch_simple_column_chart.R
@@ -8,6 +8,7 @@
 #' @param tooltip Text to be shown by tooltiop as a string
 #' @param xtitle x-axis title as a string
 #' @param ytitle y-axis title as a string
+#' @param colors Add colors through aes, provide name of the indicator with hexcolors
 #'
 #' @description A fct function
 #'
@@ -15,72 +16,140 @@
 #'
 #' @noRd
 
-hch_simple_column_chart <- function(data_passed,x_axis,y_axis,title,flname,tooltip,xtitle,ytitle){
+hch_simple_column_chart <- function(data_passed,x_axis,y_axis,title,flname,
+                                    tooltip,xtitle,ytitle,colors = "#181f86"){
 
-  highcharter::hchart(data_passed,
-                      "column",
-                      highcharter::hcaes(x = .data[[x_axis]], y = .data[[y_axis]]),
-                      color = "#181f86"
-  ) %>%
-    highcharter::hc_chart(
-      backgroundColor = "#ffffff"
+  if(!stringr::str_detect(colors, "#")){
+    highcharter::hchart(data_passed,
+                        "column",
+                        highcharter::hcaes(x = .data[[x_axis]], y = .data[[y_axis]],
+                                           color = .data[[colors]])
     ) %>%
-    highcharter::hc_plotOptions(
-      series = list(
-        pointPadding = 0,
-        groupPadding = 0.05
-      )
-    ) %>%
-    highcharter::hc_exporting(
-      enabled = TRUE,
-      buttons = list(
-        contextButton = list(
-          menuItems = c("viewFullscreen", "printChart", "separator", "downloadPNG",
-                        "downloadJPEG", "downloadSVG","separator", "downloadCSV")
+      highcharter::hc_chart(
+        backgroundColor = "#ffffff"
+      ) %>%
+      highcharter::hc_plotOptions(
+        series = list(
+          pointPadding = 0,
+          groupPadding = 0.05
         )
-      ),
-      filename = flname
+      ) %>%
+      highcharter::hc_exporting(
+        enabled = TRUE,
+        buttons = list(
+          contextButton = list(
+            menuItems = c("viewFullscreen", "printChart", "separator", "downloadPNG",
+                          "downloadJPEG", "downloadSVG","separator", "downloadCSV")
+          )
+        ),
+        filename = flname
+      ) %>%
+      highcharter::hc_title(
+        text = title,
+        margin = 20,
+        align = "left",
+        style = list(color = "#000000", useHTML = TRUE)
+      )  %>%
+      highcharter::hc_chart(zoomType = "x") %>%
+      highcharter::hc_xAxis(title = list(text = xtitle),
+                            scrollbar = list(enabled = T),
+                            labels = list(
+                              rotation = -90,
+                              step = 1,
+                              style = list(
+                                fontSize = 14,
+                                fontWeight = "bold",
+                                color = "#000000"
+                              )
+                            )) %>%
+      highcharter::hc_yAxis(title = list(text = ytitle,
+                                         style = list(
+                                           fontSize = 16,
+                                           fontWeight = "bold",
+                                           color = "#000000"
+                                         )),
+                            labels = list(
+                              style = list(
+                                fontSize = 14,
+                                fontWeight = "bold",
+                                color = "#000000"
+                              )
+                            )) %>%
+      highcharter::hc_tooltip(
+        pointFormat = tooltip,
+        style = list(
+          fontSize = 16
+        ),
+        headerFormat = '<span style="font-size: 18px"><strong>{point.key}</strong></span><br/>'
+      )%>%
+      highcharter::hc_caption(text = NULL #cap_charts
+      )
+  }else{
+    highcharter::hchart(data_passed,
+                        "column",
+                        highcharter::hcaes(x = .data[[x_axis]], y = .data[[y_axis]]),
+                        color = "#181f86"
     ) %>%
-    highcharter::hc_title(
-      text = title,
-      margin = 20,
-      align = "left",
-      style = list(color = "#000000", useHTML = TRUE)
-    )  %>%
-    highcharter::hc_chart(zoomType = "x") %>%
-    highcharter::hc_xAxis(title = list(text = xtitle),
-                          scrollbar = list(enabled = T),
-                          labels = list(
-                            rotation = -90,
-                            step = 1,
-                            style = list(
-                              fontSize = 14,
-                              fontWeight = "bold",
-                              color = "#000000"
-                            )
-                          )) %>%
-    highcharter::hc_yAxis(title = list(text = ytitle,
-                                       style = list(
-                                         fontSize = 16,
-                                         fontWeight = "bold",
-                                         color = "#000000"
-                                       )),
-                          labels = list(
-                            style = list(
-                              fontSize = 14,
-                              fontWeight = "bold",
-                              color = "#000000"
-                            )
-                          )) %>%
-    highcharter::hc_tooltip(
-      pointFormat = tooltip,
-      style = list(
-        fontSize = 16
-      ),
-      headerFormat = '<span style="font-size: 18px"><strong>{point.key}</strong></span><br/>'
-    )%>%
-    highcharter::hc_caption(text = NULL #cap_charts
-                            )
+      highcharter::hc_chart(
+        backgroundColor = "#ffffff"
+      ) %>%
+      highcharter::hc_plotOptions(
+        series = list(
+          pointPadding = 0,
+          groupPadding = 0.05
+        )
+      ) %>%
+      highcharter::hc_exporting(
+        enabled = TRUE,
+        buttons = list(
+          contextButton = list(
+            menuItems = c("viewFullscreen", "printChart", "separator", "downloadPNG",
+                          "downloadJPEG", "downloadSVG","separator", "downloadCSV")
+          )
+        ),
+        filename = flname
+      ) %>%
+      highcharter::hc_title(
+        text = title,
+        margin = 20,
+        align = "left",
+        style = list(color = "#000000", useHTML = TRUE)
+      )  %>%
+      highcharter::hc_chart(zoomType = "x") %>%
+      highcharter::hc_xAxis(title = list(text = xtitle),
+                            scrollbar = list(enabled = T),
+                            labels = list(
+                              rotation = -90,
+                              step = 1,
+                              style = list(
+                                fontSize = 14,
+                                fontWeight = "bold",
+                                color = "#000000"
+                              )
+                            )) %>%
+      highcharter::hc_yAxis(title = list(text = ytitle,
+                                         style = list(
+                                           fontSize = 16,
+                                           fontWeight = "bold",
+                                           color = "#000000"
+                                         )),
+                            labels = list(
+                              style = list(
+                                fontSize = 14,
+                                fontWeight = "bold",
+                                color = "#000000"
+                              )
+                            )) %>%
+      highcharter::hc_tooltip(
+        pointFormat = tooltip,
+        style = list(
+          fontSize = 16
+        ),
+        headerFormat = '<span style="font-size: 18px"><strong>{point.key}</strong></span><br/>'
+      )%>%
+      highcharter::hc_caption(text = NULL #cap_charts
+      )
+  }
 
 
 
