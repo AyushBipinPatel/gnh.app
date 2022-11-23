1d85da55151e0776a37ca0975737b638221c1618
AyushBipinPatel <ayush.ap58@gmail.com>
2022-11-23 18:11:26 +0530

    funciton to create pie chart

---
+++ R/fct_hch_simple_pie_chart.R
@@ -0,0 +1,92 @@
+#' hch_simple_pie_chart
+#'
+#' @param data_passed A dataframe that has required columns to create the column chart
+#' @param x_axis Name of the column in the dataframe as string for x axis
+#' @param y_axis Name of the column in the dataframe as string for y axis
+#' @param title A string for title of the chart
+#' @param flname A string to save the chart
+#' @param tooltip Text to be shown by tooltiop as a string
+#' @param xtitle x-axis title as a string
+#' @param ytitle y-axis title as a string
+#' @param colors Add colors through aes, provide name of the indicator with hexcolors
+#'
+#' @description A fct function
+#'
+#' @return The return value, if any, from executing the function.
+#'
+#' @noRd
+
+
+hch_simple_pie_chart <- function(data_passed,x_axis,y_axis,title,flname,
+                                    tooltip,xtitle,ytitle,colors){
+
+
+    highcharter::hchart(data_passed,
+                        "pie",
+                        highcharter::hcaes(x = .data[[x_axis]], y = .data[[y_axis]],
+                                           color = .data[[colors]])
+    ) %>%
+      highcharter::hc_chart(
+        backgroundColor = "#ffffff"
+      ) %>%
+      highcharter::hc_plotOptions(
+        series = list(
+          pointPadding = 0,
+          groupPadding = 0.05
+        )
+      ) %>%
+      highcharter::hc_exporting(
+        enabled = TRUE,
+        buttons = list(
+          contextButton = list(
+            menuItems = c("viewFullscreen", "printChart", "separator", "downloadPNG",
+                          "downloadJPEG", "downloadSVG","separator", "downloadCSV")
+          )
+        ),
+        filename = flname
+      ) %>%
+      highcharter::hc_title(
+        text = title,
+        margin = 20,
+        align = "left",
+        style = list(color = "#000000", useHTML = TRUE)
+      )  %>%
+      highcharter::hc_chart(zoomType = "x") %>%
+      highcharter::hc_xAxis(title = list(text = xtitle),
+                            scrollbar = list(enabled = T),
+                            labels = list(
+                              rotation = -90,
+                              step = 1,
+                              style = list(
+                                fontSize = 14,
+                                fontWeight = "bold",
+                                color = "#000000"
+                              )
+                            )) %>%
+      highcharter::hc_yAxis(title = list(text = ytitle,
+                                         style = list(
+                                           fontSize = 16,
+                                           fontWeight = "bold",
+                                           color = "#000000"
+                                         )),
+                            labels = list(
+                              style = list(
+                                fontSize = 14,
+                                fontWeight = "bold",
+                                color = "#000000"
+                              )
+                            )) %>%
+      highcharter::hc_tooltip(
+        pointFormat = tooltip,
+        style = list(
+          fontSize = 16
+        ),
+        headerFormat = '<span style="font-size: 18px"><strong>{point.key}</strong></span><br/>'
+      )%>%
+      highcharter::hc_caption(text = NULL #cap_charts
+      )
+
+
+
+
+}
