9473a1ef219b5e24074ae6f09ad0754498b7728c
AyushBipinPatel <ayush.ap58@gmail.com>
2023-01-12 16:30:48 +0530

    changes suggested by tshoki, refer sheet row 7

--- R/fct_add_landing_page_info.R
+++ R/fct_add_landing_page_info.R
@@ -12,13 +12,22 @@ add_landing_page_info <- function() {
     shiny::tags$h1("About Gross National Happiness Index"),
     shiny::tags$div(
       shiny::tags$p(
-        "The phrase ‘gross national happiness’ was first coined by the 4th King of Bhutan, King Jigme Singye Wangchuck, in 1972 when he declared, “Gross National Happiness is more important than Gross Domestic Product.” The concept implies that sustainable development should take a holistic approach towards notions of progress and give equal importance to non-economic aspects of wellbeing. The king was awarded the Blue Planet Prize in the year 2022. This prize is awarded as a recognition of scientific research and its applicaitons that have helped to provide solution for global environmental problems."
+        "The development concept of Gross National Happiness (GNH) was first articulated by the Fourth King of Bhutan, His Majesty Jigme Singye Wangchuck in the mid-1970s. Questioning the then prevailing assumption that material wealth alone could  not deliver happiness and well-being, he re-defined Bhutan's developmental pathway and centralise happiness and wellbeing as the ultimate end goal of any developmental strategy. Building on this legacy, the current King has further translated GNH into policy directions to the nation's overarching, long-term strategies and five-year plans."
       ),
       shiny::tags$p(
-        "Since then the idea of Gross National Happiness (GNH) has influenced Bhutan’s economic and social policy, and also captured the imagination of others far beyond its borders. In creating the Gross National Happiness Index, Bhutan sought to create a measurement tool that would be useful for policymaking and create policy incentives for the government, NGOs and businesses of Bhutan to increase GNH."
+        "GNH is conceptualised through the nine domains including psychological wellbeing, health, time use and balance, education, cultural diversity and resilience, good governance, community vitality, ecological diversity and resilience, and living standards. Together, these domains attempt to paint a multi-coloured picture of happiness and wellbeing in Bhutan."
+      ),
+      shiny::div(
+        shiny::tags$img("Get an SVG or high res image for the mandala picture of the nine domains")
       ),
       shiny::tags$p(
-        "The GNH Index includes both traditional areas of socio-economic concern such as living standards, health and education and less traditional aspects of culture and psychological wellbeing. It is a holistic reflection of the general wellbeing of the Bhutanese population rather than a subjective psychological ranking of ‘happiness’ alone."
+        "This domain based GNH framework also provides the structure for the 33 indicators of GNH to track the country's overall progress. The Gross National Happiness Survey is carried out every five years to capture responses across these indicators from a sample of the population. Using the Alkire Foster methodology of the multidimensional poverty measure, Bhutan developed the GNH Index to provide positive policy incentives and help improve conditions of happiness and wellbeing."
+      ),
+      shiny::tags$p(
+        "GNH Index is computed through two steps, the identification step where in happy persons are identified by distinguishing from the not-yet-happy individuals through the application of 'sufficiency thresholds' and the aggregation step, by which data on the happy persons are aggregated to create the Index. Essentially, the GNH measurement categorises persons into four groups; unhappy, narrowly happy, extensively happy, and deeply happy. The analysis explores the happiness people enjoy already, then focuses on how policies can increase happiness and sufficiency among the unhappy and narrowly happy people."
+      ),
+      shiny::tags$p(
+        "Each of the domains are equally weighted and within domains, indicators also have relatively equal weighting. However, those indicators that have exibited higher statistical reliability and objectiveness have been given slightly higher weighting."
       ),
       shiny::tags$h2("Construciton of the GNH Index"),
       shiny::tags$strong(shiny::tags$p("The GNH index includes nine domains.")),
